e3c04e7ed18fa024b1eab5535748bce0c3683ded
Jonas Charrier <jonas.charrier@etudiant.univ-lr.fr>
2024-12-05 23:01:50 +0100

    Add credit page

--- frontend/src/Pages/Credits/Credits.tsx
+++ frontend/src/Pages/Credits/Credits.tsx
@@ -1,9 +1,14 @@
 import CreditName from "../../Components/CreditName/CreditName";
+import './Credits.css'
 
 const Credits = () => {
   return (
-    <div>
-      <CreditName firstName="Manus" lastName="Dyslexius"/>
+    <div className="credits">
+      <CreditName firstName="Emmanuel" lastName="Bouldoires"/>
+      <CreditName firstName="Ivan" lastName="Bortnik"/>
+      <CreditName firstName="Dorian" lastName="Duprat"/>
+      <CreditName firstName="Alexis" lastName="Piekarz"/>
+      <CreditName firstName="Jonas" lastName="Charrier"/>
     </div>
   )
 };
