39e7cdb229976a95a1155ac93ecfeaf9baccc0ea
Jonas Charrier <jonas.charrier@etudiant.univ-lr.fr>
2024-12-05 23:53:34 +0100

    Add layout

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,19 +1,22 @@
 import { Outlet, Link } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
+import './Layout.css'
+import blob1 from './assets/blob1.svg'
+import blob2 from './assets/blob2.svg'
+import blob3 from './assets/blob3.svg'
+import blob4 from './assets/blob4.svg'
 
 const Layout = () => {
   return (
     <>
+      <img src={blob1} className="blob1"/>
+      <img src={blob2} className="blob2"/>
+      <img src={blob3} className="blob3"/>
+      <img src={blob4} className="blob4"/>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/credits">Blogs</Link>
-          </li>
-        </ul>
+        <Link to="/" className="returnButton"><FontAwesomeIcon icon={faArrowLeftLong} color="black" /></Link>
       </nav>
-
       <Outlet />
     </>
   )
