060907f0d6ff5819ccc648f812ddc1f9e9995b2f
Jonas Charrier <jonas.charrier@etudiant.univ-lr.fr>
2024-12-06 05:54:50 +0100

    Add podcasts

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router";
+import podcast1 from "../../assets/podcast1.mp3"
+import podcast2 from "../../assets/podcast2.mp3"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPodcast } from "@fortawesome/free-solid-svg-icons";
+import { useRef } from "react";
 
 const Home = () => {
+    const podcast1Elm = useRef<HTMLAudioElement>(null);
+    const podcast2Elm = useRef<HTMLAudioElement>(null);
+
     return <div className="flex flex-col justify-center items-center py-12 z-10 h-screen">
         <div className="flex flex-col justify-center items-center h-3/4 my-12">
             <img className="size-64 w-auto" src="../src/assets/home_logo.png" alt="logo" />
@@ -10,10 +18,18 @@ const Home = () => {
         </div>
 
         <div className="flex justify-center gap-4 w-2/3">
-            <div className="bg-blue-600 text-xl text-white py-3 px-12 flex-1 w-0 rounded-full text-center transition-all hover:bg-blue-400 hover:shadow-lg hover:scale-110">Crédits</div>
-            <div className="bg-blue-600 text-xl text-white py-3 px-12 flex-1 w-0 rounded-full text-center transition-all hover:bg-blue-400 hover:shadow-lg hover:scale-110">Collection</div>
-            <div className="bg-blue-600 text-xl text-white py-3 px-12 flex-1 w-0 rounded-full text-center transition-all hover:bg-blue-400 hover:shadow-lg hover:scale-110">Panthéon</div>
+            <Link to="/credit"><div className="bg-blue-600 text-xl text-white py-3 px-12 flex-1 w-0 rounded-full text-center transition-all hover:bg-blue-400 hover:shadow-lg hover:scale-110">Crédits</div></Link>
+            <Link to="/404"><div className="bg-blue-600 text-xl text-white py-3 px-12 flex-1 w-0 rounded-full text-center transition-all hover:bg-blue-400 hover:shadow-lg hover:scale-110">Collection</div></Link>
+            <Link to="/pantheon"><div className="bg-blue-600 text-xl text-white py-3 px-12 flex-1 w-0 rounded-full text-center transition-all hover:bg-blue-400 hover:shadow-lg hover:scale-110">Panthéon</div></Link>
         </div>
+        <audio ref={podcast1Elm} src={podcast1}></audio>
+        <button onClick={() => {
+            podcast1Elm.current?.paused ? podcast1Elm.current?.play() : podcast1Elm.current?.pause()
+        }}><FontAwesomeIcon icon={faPodcast} color="blue"/></button>
+        <audio ref={podcast2Elm} src={podcast2}></audio>
+        <button onClick={() => {
+            podcast2Elm.current?.paused ? podcast2Elm.current?.play() : podcast2Elm.current?.pause()
+        }}><FontAwesomeIcon icon={faPodcast} color="blue"/></button>
     </div>
 }
 
