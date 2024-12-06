e6ea063f329486e2e885baa0d29daebdac05a7a0
alexis piekarz <alexis.piekarz@etudiant.univ-lr.fr>
2024-12-06 01:02:48 +0100

    add game API

--- backend/index.ts
+++ backend/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import sqlite3 from "sqlite3";
 
 // Importation routes API (on met .js parce que ça marche que comme ça)
 import connectionRoutes from "./src/routes/connectionsRoutes.js";
@@ -9,22 +8,10 @@ import pantheonRoutes from "./src/routes/pantheonRoutes.js";
 import readTimesRoutes from "./src/routes/readTimesRoutes.js;
 import scoresRoutes from "./src/routes/scoresRoutes.js";
 
-const db = new sqlite3.Database('./src/database/database.db');
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (_req, res) => {
-    db.serialize(() => {
-        db.all('SELECT rowid AS id, info FROM lorem', (_err, rows: { id: number, info: string }[]) => {
-            if (_err) {
-                res.status(500).send("Error retrieving data");
-            } else {
-                res.json(rows);
-            }
-        });
-    });
-});
 
 app.use("/sherlock/connections", connectionRoutes);
 app.use("/sherlock/games", gamesRoutes);
--- backend/src/repositories/GameRepository.ts
+++ backend/src/repositories/GameRepository.ts
@@ -1,5 +1,19 @@
-import { db } from '../utils/database.js';
+import { db } from '../utils/database';
 
 export class GameRepository {
-    // TODO: Implement the methods to interact with the database
+    public async getAllGames(): Promise<number> {
+        const result = await db.get('SELECT COUNT(*) FROM games');
+        return result?.count || 0;
+    }
+
+    public async postGame(): Promise<number> {
+        const result = await db.run('INSERT INTO games DEFAULT VALUES');
+        return result.lastID;
+    }
+
+    public async updateGame(gameId: number): Promise<number> {
+        const result = await db.run('UPDATE games SET updated_at = CURRENT_TIMESTAMP WHERE id = ?', gameId);
+        return result.changes;
+    }
+
 }
